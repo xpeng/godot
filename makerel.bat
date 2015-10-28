25d310a803592c6ae3050755d69df051c15c3207
sanikoyes <sanikoyes@163.com>
2015-10-29 00:42:19 +0800

    Add missing lua/SCsub

--- makerel.bat
+++ makerel.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" && d:\python27\scons p=windows target=debug_release
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" && d:\python27\scons p=windows target=release_debug lua=yes
